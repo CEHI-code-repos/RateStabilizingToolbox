4bf3ebd0d37a649585b47fabac45081436142471
Ryan Zomorrodi <ryanzomorrodi@gmail.com>
2025-09-20 12:06:14 -0500

    Document rst.py

--- model/rst.py
+++ model/rst.py
@@ -7,8 +7,6 @@ import model
 
 AGE_GROUPS = ["0-4", "5-14", "15-24", "25-34", "35-44", "45-54", "55-64", "65-74", "75-84", "85up"]
 
-# this is a RST implementation without the arcgis dependency, should allow for easier testing
-# there will not be as many checks on the data, so maybe run it in arcgis pro first
 def rst(
     input_table_path: str, 
     input_table_fields: Dict[str, str], 
@@ -19,6 +17,78 @@ def rst(
     std_pop_year: Optional[int] = None, 
     std_age_groups: Optional[Dict[str, List[str]]] = None
 ) -> pd.DataFrame:
+    """Get RST estimates
+
+    Parameters
+    ----------
+    input_table_path : str
+        The file location of the table containing event and population data
+        at the region or region-age group level. Geodatabase or geopackage
+        features can be specified as "db.gdb/table" or "pkg.gpkg/table"
+    input_table_fields : Dict[str, str]
+        Dictionary containing "region_id", "event_count", and 
+        "population_count" keys which specify their respective field names 
+        within the input table.
+    input_feature_path : str
+        The file location of the table containing event and population data
+        at the region or region-age group level. Geodatabase or geopackage
+        features can be specified as "db.gdb/table" or "pkg.gpkg/table"
+    input_table_fields : Dict[str, str]
+        Dictionary containing "region_id" key which specifies the region id
+        field name within the input feature.
+    additional_options : Dict[str, Union[float, int]]
+        Dictionary containing "ci" key which specifies level of confidence
+        used to generate confidence intervals (e.g. 0.95), "rate_per" key 
+        which specifies the denomerator of output rates (e.g. 100,000), and
+        "n_years" key which specifies the total number of years of data
+        represented by the input table events.
+    age_group_field : str, optional
+        Name of age group field within the input table.
+    std_pop_year : int, optional
+        Standard population year used to age adjust output rates. 2000 or 
+        2010.
+    std_age_groups : Dict[str, List[str]], optional
+        Dictionary with keys representing the names of groups to age
+        standardize for (e.g. "65up") and values being a list of 
+        constituent age groups (e.g. ["65-74", "75-84", "85up"])
+
+    Returns
+    -------
+    pd.DataFrame
+        A dataframe containing the output rates.
+
+    Notes
+    _______
+    This is a RST implementation without the arcgis dependency, should allow for easier testing
+    there will not be as many checks on the data, so maybe run it in ArcGIS Pro first.
+
+    Examples
+    --------
+    >>> rst(
+    >>>     input_table_path = "data.gdb/mi_joined_event_pop_strat",
+    >>>     input_table_fields = {
+    >>>         "region_id": "GEOID",
+    >>>         "event_count": "EventCount",
+    >>>         "population_count": "PopulationCount"
+    >>>     },
+    >>>     input_feature_path = "data.gdb/mi_carto",
+    >>>     input_feature_fields = {
+    >>>         "region_id": "GEOID"
+    >>>     },
+    >>>     additional_options = {
+    >>>         "ci": 0.95,
+    >>>         "rate_per": 100_000,
+    >>>         "n_years": 1
+    >>>     },
+    >>>     age_group_field = "AgeGroup",
+    >>>     std_pop_year = 2000,
+    >>>     std_age_groups = {
+    >>>         "35-64": ["35-44", "45-54", "55-64"],
+    >>>         "35up": ["35-44", "45-54", "55-64", "65-74", "75-84", "85up"],
+    >>>         "65up": ["65-74", "75-84", "85up"]
+    >>>     }
+    >>> )
+    """
     input_table_id_name = input_table_fields["region_id"]
     input_table_event_name = input_table_fields["event_count"]
     input_table_pop_name = input_table_fields["population_count"]
@@ -98,55 +168,3 @@ def calculate_adj(feature: gpd.GeoDataFrame) -> list[list[int]]:
         adjacency[i] = neighbors
 
     return adjacency
-
-rst(
-    input_table_path = "Error_RST_Datasets.gdb/Hispanic_Diabetes",
-    input_table_fields = {
-        "region_id": "fips_txt",
-        "event_count": "death",
-        "population_count": "population_new"
-    },
-    input_feature_path = "Error_RST_Datasets.gdb/wa_carto_boundaries",
-    input_feature_fields = {
-        "region_id": "GEOID"
-    },
-    additional_options = {
-        "ci": 0.95,
-        "rate_per": 100_000,
-        "n_years": 1
-    },
-    age_group_field = "new_age_group",
-    std_pop_year = 2000,
-    std_age_groups = {
-        "35-64": ["35-44", "45-54", "55-64"],
-        "35up": ["35-44", "45-54", "55-64", "65-74", "75-84", "85up"],
-        "65up": ["65-74", "75-84", "85up"]
-    }
-)
-
-# rst(
-#     input_table_path = "data.gdb/mi_joined_event_pop_strat",
-#     input_table_fields = {
-#         "region_id": "GEOID",
-#         "event_count": "EventCount",
-#         "population_count": "PopulationCount"
-#     },
-#     input_feature_path = "data.gdb/mi_carto",
-#     input_feature_fields = {
-#         "region_id": "GEOID"
-#     },
-#     additional_options = {
-#         "ci": 0.95,
-#         "rate_per": 100_000,
-#         "n_years": 1
-#     },
-#     age_group_field = "AgeGroup",
-#     std_pop_year = 2000,
-#     std_age_groups = {
-#         "35-64": ["35-44", "45-54", "55-64"],
-#         "35up": ["35-44", "45-54", "55-64", "65-74", "75-84", "85up"],
-#         "65up": ["65-74", "75-84", "85up"]
-#     }
-# )
-
-
